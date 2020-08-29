fe8d98caeb37b753135eda3b5c8e11b837e38020
Harry Chan <harry.chan@mensa.org.hk>
2020-08-29 11:49:20 +0800

    added mocking example

--- util/test/test_util_functions.py
+++ util/test/test_util_functions.py
@@ -1,5 +1,5 @@
 import pytest
-from util.util_functions import summation
+from util.util_functions import summation, sum_list
 
 
 def test_summation():
@@ -10,7 +10,7 @@ def test_summation():
     assert result == expected_result
 
 
-@pytest.mark.parametrize("a,b,expected_result", [(3, 5, 8), (6, 9, 42), (2, 4, 6)])
+@pytest.mark.parametrize('a,b,expected_result', [(3, 5, 8), (6, 9, 15), (2, 4, 6)])
 def test_summation_by_using_parameterized_test(a, b, expected_result):
     result = summation(a, b)
     assert result == expected_result
@@ -22,9 +22,26 @@ def data_for_test():
 
 
 def test_summation_by_using_fixture(data_for_test):
-    print("\ndata_for_test: {}\n".format(data_for_test))
+    print('\ndata_for_test: {}\n'.format(data_for_test))
     a = data_for_test[0]
     b = data_for_test[1]
     expected_result = data_for_test[2]
     result = summation(a, b)
     assert result == expected_result
+
+
+def test_sum_list():
+    data = [1, 2, 3]
+    expected_result = 1 + 2 + 3
+    result = sum_list(data)
+    assert result == expected_result
+
+
+def test_sum_list_with_mock(mocker):
+    data = [1, 2, 3]
+    expected_result = 10
+    # to make the 'summation' function to always return same result
+    mocker.patch('util.util_functions.summation', return_value=expected_result)
+    result = sum_list(data)
+    assert result == expected_result
+
--- util/util_functions.py
+++ util/util_functions.py
@@ -1,2 +1,11 @@
+from functools import reduce
+import logging
+
+
 def summation(a, b):
+    logging.info('a: {}, b: {}'.format(a, b))
     return a + b
+
+
+def sum_list(data):
+    return reduce(summation, data)
